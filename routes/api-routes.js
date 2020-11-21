a2fb5f6b4f89b51ccc7eb2faccd31c9ac24fd0b4
Henry Logique <hjlogique@yahoo.com>
2020-11-20 21:09:55 -0500

    updating my files to my branch

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,7 +20,8 @@ module.exports = function(app) {
   app.post("/api/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
+      state: req.body.state
     })
       .then(() => {
         res.redirect(307, "/api/login");
